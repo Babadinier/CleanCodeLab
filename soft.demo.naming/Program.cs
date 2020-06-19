1716d7e4d1e5ff148578f7a07aab76a5163cb68e
Guillaume BADINIER <gbadinier@saretec.fr>
2020-06-19 20:41:47 +0200

    Add logic in soft.demo.naming

--- soft.demo.naming/Program.cs
+++ soft.demo.naming/Program.cs
@@ -6,7 +6,28 @@ namespace soft.demo.naming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fn = "Toto";
+            string ln = "BLANC";
+
+            var usrArray =  new UserInfo[1];
+            usrArray[0] = new UserInfo(fn, ln);
+
+            for(int i = 0; i < usrArray.Length; i++)
+            {
+                Console.WriteLine("Hello " + usrArray[i].LastName + " " + usrArray[i].FistName);
+            }
+        }
+
+        public class UserInfo
+        {
+            public string FistName { get; set; }
+            public string LastName { get; set; }
+
+            public UserInfo(string firstName, string lastName)
+            {
+                FistName = firstName;
+                LastName = lastName;
+            }
         }
     }
 }
