141fc504fcc733c0eb8daed25e579c5faaaeea72
Ignacio ROGNONI <rognoni.ignacio@gmail.com>
2020-06-23 20:48:08 +0200

    Add project soft.demo.structure

---
+++ soft.demo.structure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soft.demo.structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
