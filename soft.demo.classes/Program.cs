b7b9fcab57ab0816de2c617d23e07955e70f7de5
Guillaume BADINIER <gbadinier@saretec.fr>
2020-06-25 19:24:05 +0200

    Change name of soft.demo.class

---
+++ soft.demo.classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soft.demo.classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
