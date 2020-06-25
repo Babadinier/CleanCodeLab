bcedeec654ee2c7bb712318ddadf51857d404d42
Guillaume BADINIER <gbadinier@saretec.fr>
2020-06-25 20:57:12 +0200

    Add business logic to soft.demo.lsp

--- soft.demo.classes/soft.demo.lsp/Program.cs
+++ soft.demo.classes/soft.demo.lsp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace soft.demo.lsp
 {
@@ -6,7 +7,41 @@ namespace soft.demo.lsp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+
+            SumCalculator sum = new SumCalculator(numbers);
+            System.Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+
+            System.Console.WriteLine();
+
+            EvenNumbersSumCalculator evenSum = new EvenNumbersSumCalculator(numbers);
+            System.Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
+
+            // What is the result ? 
+            SumCalculator evenSum2 = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSum2.Calculate()}");
         }
+    }
+
+    public class SumCalculator
+    {
+        protected readonly int[] _numbers;
+
+        public SumCalculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public int Calculate() => _numbers.Sum();
+    }
+
+    public class EvenNumbersSumCalculator : SumCalculator
+    {
+        public EvenNumbersSumCalculator(int[] numbers)
+            : base(numbers)
+        {
+        }
+
+        public new int Calculate() => _numbers.Where(x => x % 2 == 0).Sum();
     }
 }
