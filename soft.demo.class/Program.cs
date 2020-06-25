8951835d433e87a9c9b02f09fb7c474bcab6ac26
Guillaume BADINIER <gbadinier@saretec.fr>
2020-06-25 19:22:29 +0200

    Add new project soft.demo.class

---
+++ soft.demo.class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soft.demo.class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
