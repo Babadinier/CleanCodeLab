c988125e82ea56578ba15a9c60f01e53209d5986
Guillaume BADINIER <gbadinier@saretec.fr>
2020-06-25 20:52:05 +0200

    Add business logic in soft.demo.dip

--- soft.demo.classes/soft.demo.dip/Program.cs
+++ soft.demo.classes/soft.demo.dip/Program.cs
@@ -6,7 +6,55 @@ namespace soft.demo.dip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var employee = new Employee("Bertrand");
+
+            var employeeService = new EmployeeService();
+
+            employeeService.AddEmployee(employee);
         }
     }
+
+    public class Employee {
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class EmployeeService
+    {
+        public void AddEmployee(Employee employee)
+        {
+            var database = new Database();
+            
+            if (database.CurrentThread < database.MaxThreads)
+            {
+                database.Add(employee);
+                database.Save();
+            }
+            else {
+                /* business logic */
+            }
+        }
+    }
+
+    // this class use EntityFramework
+    public class Database : DbContext
+    {
+        private const string connectionString = @"Server=myServerName\myInstanceName;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+        public int MaxThreads => 5;
+        public int CurrentThread => 0;
+        
+        public void Add(Employee employee) { System.Console.WriteLine($"Employee {employee.Name} added"); }
+
+        public void Save() { System.Console.WriteLine("Employee saved"); }
+
+        private void InitContext() {
+            /* init context DbContext */
+        }
+    }
+
+    public class DbContext { }
 }
